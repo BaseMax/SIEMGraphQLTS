c13b0bca409fcb96b35295686e722ef155d37d58
Alireza Naghdipour <alirezanqp@gmail.com>
2023-08-27 19:11:59 +0330

    update project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,67 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
+import { PrismaModule } from './modules/prisma/prisma.module';
+import { SecurityEventsModule } from './modules/security-events/security-events.module';
+import { SearchModule } from './modules/search/search.module';
+import { RulesModule } from './modules/rules/rules.module';
+import { DataSourceModule } from './modules/data-source/data-source.module';
+import { AlertsModule } from './modules/alerts/alerts.module';
+import { MailModule } from './modules/mail/mail.module';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { UserGroupModule } from './modules/user-group/user-group.module';
+import { APP_GUARD } from '@nestjs/core';
+import { GqlAuthGuard } from './common/guards/auth.guard';
+import { RolesGuard } from './common/guards/roles.guard';
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: join(process.cwd(), 'src/graphql'),
+      autoSchemaFile: join(process.cwd(), 'src/graphql.gql'),
+      installSubscriptionHandlers: true,
+      subscriptions: {
+        'subscriptions-transport-ws': {
+          path: '/graphql',
+          onConnect: (headersRaw: Record<string, unknown>) => {
+            // Lowercase each header key
+            const headers = Object.keys(headersRaw).reduce((dest, key) => {
+              dest[key.toLowerCase()] = headersRaw[key];
+              return dest;
+            }, {});
+            return {
+              req: {
+                headers: headers,
+              },
+            };
+          },
+        },
+      },
     }),
+    EventEmitterModule.forRoot(),
+    PrismaModule,
+    AuthModule,
+    UserModule,
+    SecurityEventsModule,
+    SearchModule,
+    RulesModule,
+    DataSourceModule,
+    AlertsModule,
+    MailModule,
+    UserGroupModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: GqlAuthGuard,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
